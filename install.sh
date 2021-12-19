9d0656cfe3e6042b84eaf387678b2bc95cb831c6
Panda <73842511+PandaCatPlayz@users.noreply.github.com>
2021-12-19 12:12:25 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 # Author: PandaCatPlayz
 # Site: https://github.com/PandaCatPlayz/Artix-Base-Installer
 
+# Go to main directory
+cd
+
 # Updates system and adds a few of my needed packages
 sudo pacman --noconfirm -Syu base-devel xorg xorg-xinit picom vim
 
