e5c2e9fcd4c8cc0f7ff6ad0711ef79a35bb312f6
Panda <73842511+PandaCatPlayz@users.noreply.github.com>
2021-12-19 12:12:00 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Basic installer for Artix linux!
+# Author: PandaCatPlayz
+# Site: https://github.com/PandaCatPlayz/Artix-Base-Installer
+
+# Updates system and adds a few of my needed packages
+sudo pacman --noconfirm -Syu base-devel xorg xorg-xinit picom vim
+
+# Installs yay
+cd 
+git clone https://aur.archlinux.org/yay-git
+cd yay-git
+makepkg -si
